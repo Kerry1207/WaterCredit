9cc88cc1009772e75355580c2556be90ffa38220
Kerry <kerry1207@users.noreply.github.com>
2024-07-14 01:39:58 +0200

    updated cors origin

--- water-credit-back-end/server.js
+++ water-credit-back-end/server.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 app.use(cookieParser());
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.setHeader('Access-Control-Allow-Origin', process.env.ENDPOINT);
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); 
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', true); 
